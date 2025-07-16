05c769736ef3e4cd80d630ce596daeb49577c49b
Pavlo Chykalo <P.Chykalo.offer@gmail.com>
2025-07-16 22:19:32 +0300

    Feat03(HW11)_Task3

--- dart_async_homework/bin/dart_async_homework.dart
+++ dart_async_homework/bin/dart_async_homework.dart
@@ -4,13 +4,24 @@ import 'package:dart_async_homework/homeworks/lesson11/Future/task2.dart';
 void main () async{
   fetchName();
   fetchAge();
-  print("Починаємо отримувати ім'я...");
+  print("Починаємо виконання асинхронних операцій...");
+
+  // --- Вимірювання часу для fetchName() ---
+  final stopwatchName = Stopwatch()..start(); // Створюємо та запускаємо секундомір
+  print("\n Отримую ім'я...");
   String name = await fetchName();
-  print("Починаємо отримувати вік...");
+  stopwatchName.stop(); // Зупиняємо секундомір
+  print("Мене звати $name");
+  print("Час виконання fetchName(): ${stopwatchName.elapsedMilliseconds} мс");
+
+  // --- Вимірювання часу для fetchAge() ---
+  final stopwatchAge = Stopwatch()..start(); // Створюємо та запускаємо секундомір
+  print("\n Отримую вік...");
   String ageString = await fetchAge();
+  stopwatchAge.stop(); // Зупиняємо секундомір
   int age = int.parse(ageString);
   String yearForm = getYearForm(age);
-  print("Мене звати $name");
   print("Мені $age $yearForm");
-  print("Кінець програми.");
+  print("Час виконання fetchAge(): ${stopwatchAge.elapsedMilliseconds} мс");
+  print("\n Кінець програми.");
 }
